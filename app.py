2bca46590e3a6677934552eb238296e63ff763bb
yuna seo <playamongthestars000@gmail.com>
2022-02-28 05:37:57 +0900

    5-2 css 죅금 수정

--- app.py
+++ app.py
@@ -33,10 +33,11 @@ def review_post():
 
     return jsonify({'msg':'등록되었습니다.'})
 
+
 @app.route("/review", methods=["GET"])
 def review_get():
     review_list = list(db.reviewcmt.find({}, {'_id': False}))
-    return jsonify({'reviewcmt':review_list})
+    return jsonify({'reviews':review_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
