cf82f9a13ffbaa440b2ff0506deef8153b84abfc
yuna seo <playamongthestars000@gmail.com>
2022-03-02 22:14:29 +0900

    0302

--- app.py
+++ app.py
@@ -1,43 +1,53 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:yunayuna@cluster0.5i0os.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route("/book", methods=["GET"])
 def book_get():
     book_list = list(db.books.find({}, {'_id': False}))
-    return jsonify({'books':book_list})
+    return jsonify({'books': book_list})
 
 
-@app.route('/review')
+@app.route('/review', methods=['GET'])
 def review():
-    return render_template('review.html')
+    isbn = request.args.get('isbn')
+    return render_template('review.html', isbn=isbn)
+
 
 @app.route("/reviews", methods=["POST"])
 def review_post():
     nickname_receive = request.form['nickname_give']
     content_receive = request.form['content_give']
-    doc = {
-        'rvnickname' : nickname_receive,
-        'rvcontent' : content_receive
-    }
-    db.reviewcmt.insert_one(doc)
+    isbn_receive = request.form['isbn_give']
+    isbn_int = int(isbn_receive)
 
-    return jsonify({'msg':'등록되었습니다.'})
+    isbn = db.books.find_one({'isbn'['value']: isbn_int})
+    print(isbn)
+    # doc = {
+    #     'nickname':nickname_receive,
+    #     'content':content_receive,
+    # }
+    # db.books.find()
+
+    return jsonify({'msg': '등록되었습니다.'})
 
 
 @app.route("/reviews", methods=["GET"])
 def review_get():
-    review_list = list(db.reviewcmt.find({}, {'_id': False}))
-    return jsonify({'reviews':review_list})
+    book_list = list(db.books.find({}, {'_id': False}))
+    return jsonify({'books': book_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
